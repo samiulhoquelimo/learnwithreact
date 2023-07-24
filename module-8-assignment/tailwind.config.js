70b7b707751a8e0822d137aa244628464af6ab92
Samiul Hoque Limo <samiul.limo@gmail.com>
2023-07-24 14:37:33 +0600

    module-8-assignment

--- module-8-assignment/tailwind.config.js
+++ module-8-assignment/tailwind.config.js
@@ -1,7 +1,9 @@
+// eslint-disable-next-line no-undef
 module.exports = {
     content: [
         'node_modules/daisyui/dist/**/*.js',
         'node_modules/react-daisyui/dist/**/*.js',
     ],
+    // eslint-disable-next-line no-undef
     plugins: [require('daisyui')],
 }
